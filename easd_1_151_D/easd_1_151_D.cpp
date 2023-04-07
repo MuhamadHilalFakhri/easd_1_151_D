1095d145b1e62c2182ecc804d34541462ca53d6b
hilal555 <muhamadhilal04@gmail.com>
2023-04-07 10:16:48 +0700

    membuat fungsi input

--- easd_1_151_D/easd_1_151_D.cpp
+++ easd_1_151_D/easd_1_151_D.cpp
@@ -4,3 +4,13 @@
 // 3. 
 // 4. Selection sort, karena konsep dari selection sort adalah mencari nilai terkecil dan menukarnya dengan indeks paling awal pada setiap tahap
 // 5. - Quadratic = Bubble sort, selection sort, insertion sort - Loglinear = Quick sort, merge sort, dam sort
+
+#include <iostream>
+using namespace std;
+
+void Input(int arr[], int n) {
+    cout << "Masukkan data : ";
+    for (int MHF = 0; MHF < n; MHF++) {
+        cin >> arr[MHF];
+    }
+}
