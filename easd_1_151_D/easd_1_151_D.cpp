5af3a08daee7bb1bd48533f024a437bb42b1c4cd
hilal555 <muhamadhilal04@gmail.com>
2023-04-07 10:08:32 +0700

    Add project files.

---
+++ easd_1_151_D/easd_1_151_D.cpp
@@ -0,0 +1,6 @@
+// Jawaban
+// 1. Karena tujuan algoritma untuk memberikan proses atau serangkaian aturan yang harus diikuti dalam perhitungan atau operasi pemecahan masalah lainnya terutama oleh komputer, dan algoritma diperlukan untuk menyelesaikan suatu masalah sebab algoritma adalah langkah-langkah sistematis yang diatur secara logis untuk menyelesaikan suatu tugas atau masalah dengan efisien dan efektif.
+// 2. Static dan dynamic
+// 3. 
+// 4. Selection sort, karena konsep dari selection sort adalah mencari nilai terkecil dan menukarnya dengan indeks paling awal pada setiap tahap
+// 5. - Quadratic = Bubble sort, selection sort, insertion sort - Loglinear = Quick sort, merge sort, dam sort
